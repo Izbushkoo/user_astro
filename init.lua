2d73e1f95e5b33ce5ebf7d6885d653d2630e4723
izbushko <izbushko@pop-os.localdomain>
2023-05-29 20:00:22 +0200

    some custom

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "oxocarbon",
+  colorscheme = "gruvbox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
